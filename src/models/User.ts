a306e8f03af9be199aeef9d53b8d13d186655d48
Pathum Chinthaka <pathum.chinthaka@intellimorph.ai>
2025-07-17 11:42:46 +0530

    new role type

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,10 +1,20 @@
-import { Schema, model } from 'mongoose';
+import { Schema, model } from "mongoose";
+import { UserRole } from "../types/enum/userRole.enum";
 
-const UserSchema = new Schema({
-  email: { type: String, required: true, unique: true },
-  mobile: { type: String, required: true, unique: true },
-  password: { type: String, required: true }, 
-  role: { type: String, enum: ['admin', 'librarian'], default: 'librarian' },
-}, { timestamps: true });
+const UserSchema = new Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    mobile: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: {
+      type: String,
+      enum: Object.values(UserRole),
+      default: UserRole.LIBRARIAN,
+    },
+  },
+  { timestamps: true }
+);
 
-export const User = model('User', UserSchema);
+export const User = model("User", UserSchema);
