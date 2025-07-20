428eee468e7614a79f7df5ddf1d06ad55e669208
Pathum Chinthaka <pathum.chinthaka@intellimorph.ai>
2025-07-20 14:29:22 +0530

    change route name

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -10,7 +10,7 @@ const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", userLogin);
-router.get("/refresh:token", refreshToken);
+router.get("/refresh-token", refreshToken);
 router.post("/logout", logoutUser);
 
 export default router;
