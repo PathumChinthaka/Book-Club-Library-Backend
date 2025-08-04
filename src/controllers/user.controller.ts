ab81665fc1ac83e1acef71ff0df8989514eaf0aa
Pathum Chinthaka <pathum.chinthaka@intellimorph.ai>
2025-08-04 11:13:31 +0530

    profile picture upload done

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { User } from "../models/User";
 import { ActiveStatus } from "../types/enum/activeStatusEnum";
+import { uploadFileToSupabase } from "../service/file/file.upload.service";
 
 export const getUserById = async (
   req: Request,
@@ -12,7 +13,7 @@ export const getUserById = async (
 
     const user = await User.findOne({
       _id: id,
-      activeStatus: ActiveStatus.Active, 
+      activeStatus: ActiveStatus.Active,
     }).select("-password");
 
     if (!user) return res.status(404).json({ message: "User not found" });
@@ -60,6 +61,37 @@ export const updateUserById = async (
       return res.status(404).json({ message: "User not found" });
 
     res.json(updatedUser);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const uploadProfilePicture = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const userId = req.params.id;
+    const file = req.file;
+
+    if (!file) {
+      return res.status(400).json({ message: "Profile picture is required" });
+    }
+
+    const url = await uploadFileToSupabase(file);
+
+    const user = await User.findByIdAndUpdate(
+      userId,
+      { profilePictureUrl: url, updatedOn: new Date() },
+      { new: true }
+    ).select("-password");
+
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    res.status(200).json({
+      user,
+    });
   } catch (err) {
     next(err);
   }
--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,9 +1,15 @@
 import express from "express";
-import { getUserById, updateUserById } from "../controllers/user.controller";
+import {
+  getUserById,
+  updateUserById,
+  uploadProfilePicture,
+} from "../controllers/user.controller";
+import { upload } from "../middleware/multer.middleware";
 
 const router = express.Router();
 
 router.get("/:id", getUserById);
 router.put("/:id", updateUserById);
+router.put("/:id/profile-picture", upload.single("file"), uploadProfilePicture);
 
 export default router;
